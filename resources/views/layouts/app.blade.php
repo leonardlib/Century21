19faaf39ff3d23427d8b9f5eb7a2e01c2256c243
Leonardo Lira Becerra <leonardolirabecerra@MacBook-Pro-de-Leonardo.local>
2017-03-30 22:06:51 -0600

    Home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,38 @@
         <script src="{{url('/js/jquery-3.2.0.js')}}"></script>
         <script src="{{url('/css/bootstrap-3/js/bootstrap.js')}}"></script>
     </head>
+    <style>
+        #logo {
+            background: #000000;
+            margin: 0;
+        }
+    </style>
     <body>
-        <div class="jumbotron" align="center">
-            <h1>@yield('cabecera')</h1>
+    <div class="jumbotron" align="center" id="logo">
+        <img src="{{url('/img/logo.png')}}" alt="">
+    </div>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li id="home"><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
         </div>
+    </nav>
     </body>
 </html>

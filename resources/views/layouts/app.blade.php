90b6a977a8ec0db4f77e4f35fb02f46433d0c662
Leonardo Lira Becerra <leonardolirabecerra@MacBook-Pro-de-Leonardo.local>
2017-05-16 08:18:41 -0500

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
                         <li id="inicio"><a href="{{url('/inicio')}}">Inicio</a></li>
                         <li class="dropdown" id="catalogos">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                Catalogos
+                                Catálogos
                                 <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
@@ -91,7 +91,7 @@
             <div class="footercontainer">
                 <div class="container">
                     <div class="row" style="text-aling: center;">
-                        <div class="col-sm-3 hidden-xs">
+                        <div class="col-md-2">
                             <img src="{{url('/img/logo.png')}}" alt="">
                             <br><br>
                             <p>
@@ -101,46 +101,67 @@
                                 <a href="http://twitter.com/c21mexico"><img src="{{url('/img/footer/twitter.webp')}}"></a>
                             </p>
                         </div>
-                        <div class="col-sm-3">
-                            <samp class="titulofooter">NOSOTROS</samp><br>
-                            <br>
-                            <a href="/mision">MISIÓN, VISIÓN Y VALORES</a><br>
-                            <br>
-                            CENTURY 21 MÉXICO<br>
-                            MIGUEL DE CERVANTES SAAVEDRA NO. 301 TORRE NORTE, PISO 15<br>
-                            COLONIA AMPLIACIÓN GRANADA<br>
-                            11520 MÉXICO, D.F.<br>
+                        <div class="col-md-2">
+                            <h4 class="seccion-footer">Nosotros</h4>
+                            <br/>
+                            <a href="{{url('/inicio')}}">Misión, Visión y Valores</a>
+                            <br/>
+                            <br/>
+                            <p class="seccion-footer">
+                                Century 21 México<br/>
+                                Miguel De Cervantes Saavedra No. 301 Torre Norte, Piso 15<br/>
+                                Colonia Ampliación Granada<br/>
+                                11520 México, D.F.<br/>
+                            </p>
                         </div>
-                        <div class="col-sm-3">
-                            <samp class="titulofooter">EXPLORA</samp><br>
-                            <br>
-                            <a href="/franquicias">FRANQUICIAS</a><br>
-                            <a href="/acercade">ACERCA DE C21MX</a><br>
-                            <a href="http://century21global.com">BÚSQUEDA INTERNACIONAL</a><br>
-                            <a href="/noticias">NOTICIAS</a><br>
-                            <a href="http://aprivada.century21mexico.com">PAGINA PRIVADA</a><br>
-                            <a href="/directorio">DIRECTORIO DE OFICINAS</a><br>
-                            <a href="/venta">PROPIEDADES EN VENTA</a><br>
-                            <a href="/renta">PROPIEDADES EN RENTA</a><br>
-                            <a href="https://eventoscentury21mexico.wordpress.com/">CONVENCIÓN NACIONAL 2017</a>
+                        <div class="col-md-2">
+                            <h4 class="seccion-footer">Catálogos</h4>
+                            <br/>
+                            <a href="{{ url('/catalogos/fraccionamientos') }}">Fraccionamiento</a><br/>
+                            <a href="{{ url('/catalogos/clientes') }}">Clientes</a><br/>
+                            <a href="{{ url('/catalogos/vendedores') }}">Vendedores</a><br/>
+                            <a href="#">Usuarios</a><br/>
                         </div>
-                        <div class="col-sm-3">
-                            <samp class="titulofooter">CONTÁCTANOS</samp><br>
-                            <br>
-                            <a href="/contactanos">CONTÁCTANOS</a><br>
-                            <a href="/enviar-comentario">ENVÍANOS UN COMENTARIO</a><br>
-                            <a href="/ofrezco-propiedad">OFREZCO PROPIEDAD</a><br>
-                            <a href="/reportar-anomalia">REPORTAR ANOMALÍA</a><br>
-                            <a href="/franquicias">SOLICITUD DE FRANQUICIA</a><br>
-                            <a href="/unete">RECLUTAMIENTO</a><br>
+                        <div class="col-md-2">
+                            <h4 class="seccion-footer">Movimientos</h4>
+                            <br/>
+                            <a href="{{url('/movimientos/solicitudes')}}">Solicitudes</a><br/>
+                            <a href="#">Pago de Solicitudes</a><br/>
+                            <a href="#">Contratos</a><br/>
+                            <a href="#">Pago de Mensualidades</a><br/>
+                            <a href="#">Mensualidades Vencidas</a><br/>
+                        </div>
+                        <div class="col-md-2">
+                            <h4 class="seccion-footer">Reportes</h4>
+                            <br/>
+                            <a href="#">Lista de Solicitudes</a><br/>
+                            <a href="#">Saldos</a><br/>
+                            <a href="#">Estado de Cuenta</a><br/>
+                            <a href="#">Saldos Vencidos</a><br/>
+                        </div>
+                        <div class="col-md-2">
+                            <h4 class="seccion-footer">Utilerias</h4>
+                            <br/>
+                            <a href="#">Respaldar Informacion</a><br/>
+                            <a href="#">Regenerar Indices</a><br/>
                         </div>
                     </div>
-                    <div class="row avisorow">
+                    <br/>
+                    <div class="row">
                         <div class="col-sm-12 aviso">
-                            <a href="/avisodeprivacidad">AVISO DE PRIVACIDAD - CENTURY 21 MÉXICO</a><br>
+                            <a href="#">Aviso de Privacidad - Century 21 México</a><br>
                         </div>
-                        <div class="col-sm-12 minaviso">
-                            Cada Oficina es de Propiedad y Operación Independiente. | ® la marca y el logotipo CENTURY 21 son propiedad de CENTURY 21 Real Estate Corporation, LLC. | Diseño: CENTURY 21 México, S.A. de C.V., Dirección de Mercadotecnia. | CENTURY 21 México, no es responsable por la exactitud o complementos de la información de los Afiliados, de los asesores y de las propiedades y de otra información provista por los franquiciatarios en este sitio. | La información puede cambiar sin previo aviso. | La opinión de algunos artículos es responsabilidad de sus autores y han sido publicados solo para fines informativos y no expresa el punto de vista de CENTURY 21 México, S.A. de C.V.
+                        <div class="col-sm-12 minaviso seccion-footer">
+                            <p>
+                                Cada Oficina es de Propiedad y Operación Independiente. | ® la marca y el logotipo
+                                CENTURY 21 son propiedad de CENTURY 21 Real Estate Corporation, LLC. | Diseño: CENTURY
+                                21 México, S.A. de C.V., Dirección de Mercadotecnia. | CENTURY 21 México, no es responsable
+                                por la exactitud o complementos de la información de los Afiliados, de los asesores y
+                                de las propiedades y de otra información provista por los franquiciatarios en este
+                                sitio. | La información puede cambiar sin previo aviso. | La opinión de algunos
+                                artículos es responsabilidad de sus autores y han sido publicados solo para fines
+                                informativos y no expresa el punto de vista de CENTURY 21 México, S.A. de C.V.
+                            </p>
                         </div>
                     </div>
                 </div>

734c125b5e479ef7e9171ec7f5695e538d47377e
Luis Arvizu <laar0696@gmail.com>
2017-05-23 02:59:51 -0500

    Reporte Saldos

--- resources/views/reportes/saldos.blade.php
+++ resources/views/reportes/saldos.blade.php
@@ -8,7 +8,7 @@
 		
 		<div class="col-md-12" role = "main" id="contenedor">
 			
-			<form class="form-horizontal" action="clientes_store" method="post">			
+				
 				
 				<br>
 				
@@ -22,12 +22,52 @@
 					</div>
 					
 					<div class="panel-body">
+
+						<center><h2>Reporte Saldos</h2></center>	
+<pre>
+
+<b>Fecha:</b> {{date('l jS \of F Y h:i:s A')}}
+
+<b>Total Saldos:</b> {{$total}}
+
+	
+</pre>												
+						<table id="tabReporteSaldos" class="table table-striped">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Cliente</th>
+									<th>Colonia</th>
+									<th>Calle</th>
+									<th>Telefono</th>
+									<th>IFE</th>
+									<th>Saldo</th>
+								</tr>
+							</thead>
+							<tbody>	
+								@foreach($saldos as $saldo)
+									<tr>
+										<td>{{$saldo->id}}</td>
+										<td>{{$saldo->nombre}}</td>
+										<td>{{$saldo->colonia}}</td>
+										<td>{{$saldo->calle}}</td>
+										<td>{{$saldo->telefono}}</td>
+										<td>{{$saldo->ife}}</td>
+										<td>{{$saldo->saldo}}</td>
+									</tr>
+								@endforeach
+							</tbody>
+						</table>
+						<div class="panel-footer">
+							<div align="right">
+								<button class="btn btn-warning" id="ver_recibo">Version Pdf</button>
+							</div>
+						</div>
 					</div>
 				
 				</div>
 
-			</form>
-		
+
 		</div>
 	
 	</div>
@@ -38,6 +78,7 @@
         $('#clientes').attr('class', 'active');
 	</script>
 
+	<script src="{{ asset('/js/reciboSaldos.js') }}"></script>
     <script src="{{ asset('/js/jquery-3.2.0.js') }}"></script>
     <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css" />
 	<script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>

c11a8c2c4856b7c8cc6e09ce01ebb3721a8adc6a
Luis Arvizu <laar0696@gmail.com>
2017-05-24 16:00:44 -0500

    .

--- public/js/clientes.js
+++ public/js/clientes.js
@@ -1,38 +1,9 @@
 $(function(){
 
-
-	
-
     $('#tabClientes').DataTable({
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.12/i18n/Spanish.json"
-        }/*,
-
-        "fnDrawCallback": function() {
-            //$('.check-estado').bootstrapToggle();
-        },
-        "scrollX": true,
-        "processing": true,
-        "serverSide": true,
-        "ajax": 'clientes.tabla',
-        "columns": [
-            {data: 'id'},
-            {data: 'nombre'},
-            {data: 'saldo'},
-            {data: 'telefono'},
-            {data: 'colonia'},
-            {data: 'ife'},
-            {data: 'id',
-                render: function(data, type, row) {
-                    return '<button type="button" class="btn btn-success boton_modificar" id="modal_"'+data+' data-toggle="modal" data-target="#modalModificar" value='+data+'>Editar</button>'
-                }
-            },
-            {data: 'id',
-                render: function(data, type, row) {
-                    return '<button type="button" class="btn btn-warning btn-circle btn-xl"><i class="glyphicon glyphicon-remove"></i></button>'
-                }
-            }
-        ],*/
+        }
     });
 
 
@@ -89,37 +60,5 @@ $(function(){
             }
         });
     });
-/*
-    $("#modalGuardar").on("click", function(){
-		var id 		= $("#modalModificar #id").val();
-   		var nombre 	= $("#modalModificar #inputNombre").val();
-   		var saldo 	= $("#modalModificar #inputSaldo").val();
-   		var telefono= $("#modalModificar #inputTelefono").val();
-   		var calle 	= $("#modalModificar #inputCalle").val();
-   		var ife 	= $("#modalModificar #inputIfe").val();
-   		var colonia = $("#modalModificar #inputColonia").val();
-   	
-    	$.ajax({
-            type: "POST",
-            url: 'clientes.editar',
-            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-            data: {
-                id: id,
-                nombre: nombre,
-                saldo: saldo,
-                telefono: telefono,
-                calle: calle,
-                ife: ife,
-                colonia: colonia
-            },
-            success: function(data) {
-            	
-            },
-            error: function(data) {
-                console.log('Error:', data);
 
-            }
-        });
-    });
-*/
 });
